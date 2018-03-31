9aa2a1d5d6333fa2ca3b77cf8402aeee380b19b3
Steve Clay <steve@mrclay.org>
2018-03-30 23:25:44 -0400

    More friendly title UX

--- src/ui/Title.js
+++ src/ui/Title.js
@@ -3,18 +3,58 @@ import {RIEInput} from 'riek';
 
 import {DEFAULT_TITLE} from "../constants";
 
-export default function Title(props) {
-  const change = props.onChange ? props.onChange : () => {};
-  const className = props.title ? 'titled' : 'untitled';
+export default class Title extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <h2 className={`Title ${className}`}>
-      “<RIEInput
-        value={props.title || DEFAULT_TITLE}
-        propName='title'
-        change={change}
-        isDisabled={!props.onChange}
-      />”
-    </h2>
-  );
+    this.state = {
+      title: props.title,
+      widget: (
+        <RIEInput
+          value={props.title || DEFAULT_TITLE}
+          propName='title'
+          change={this.handleChange}
+          isDisabled={!props.onChange}
+        />
+      ),
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.title === this.props.title
+      && nextProps.onChange === this.props.onChange
+    ) {
+      return;
+    }
+
+    this.setState({
+      title: nextProps.title,
+      widget: (
+        <RIEInput
+          value={nextProps.title || DEFAULT_TITLE}
+          propName='title'
+          change={this.handleChange}
+          isDisabled={!nextProps.onChange}
+        />
+      ),
+    });
+  }
+
+  handleChange = (title) => {
+    if (this.props.onChange) {
+      this.props.onChange(title);
+    }
+  };
+
+
+
+  render() {
+    const { widget, title } = this.state;
+    const className = title ? 'titled' : 'untitled';
+    return (
+      <h2 className={`Title ${className}`}>
+        “{widget}”
+      </h2>
+    );
+  }
 }
