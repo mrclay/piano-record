dfaba4aaa1055c7ea76be85873b2484bb91bb9b2
Steve Clay <steve@mrclay.org>
2024-02-23 00:08:22 -0500

    Use lftp for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,20 @@
 
 set -euo pipefail
 
-# SRC and DEST must have trailing slashes
-SRC=dist/
-DEST=/home1/mrclayor/public_html/piano/
-
 npm run build
 
-rsync -avzh ${SRC} mrclayor@mrclay.org:${DEST}
+# https://lftp.yar.ru/lftp-man.html
+# https://www.gnu.org/software/inetutils/manual/html_node/The-_002enetrc-file.html
+lftp galileo.iwebfusion.net <<-EOF
+  # Get the top level files
+  mirror --no-recursion -v -e -P=5 -R dist /public_html/piano
+
+  # Main assets
+  rm -r /public_html/piano/assets
+  mirror -v -e -P=5 -R dist/assets /public_html/piano/assets
+
+  # These are big files. Don't worry about time differences
+  mirror --ignore-time -v -e -P=5 -R dist/media /public_html/piano/media
+
+  bye
+EOF
