e43629c1aea34fca919b679b8fc4f64f34277476
Steve Clay <steve@mrclay.org>
2024-02-23 21:05:37 -0500

    Use scp for now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,6 @@ set -euo pipefail
 
 npm run build
 
-# https://lftp.yar.ru/lftp-man.html
-# https://www.gnu.org/software/inetutils/manual/html_node/The-_002enetrc-file.html
-lftp galileo.iwebfusion.net <<-EOF
-  # Get the top level files
-  mirror --no-recursion -v -e -P=5 -R dist /public_html/piano
+mv dist piano || echo 0
 
-  # Main assets
-  rm -r /public_html/piano/assets
-  mirror -v -e -P=5 -R dist/assets /public_html/piano/assets
-
-  # These are big files. Don't worry about time differences
-  mirror --ignore-time -v -e -P=5 -R dist/media /public_html/piano/media
-
-  bye
-EOF
+scp -r piano mrclay:/home/mrclayor/public_html/
